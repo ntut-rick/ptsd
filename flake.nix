6ed3f3d893aed2cd1f41c38f6f5d01c583f8cb21
SArthurX <sxa05z@gmail.com>
2024-06-30 00:19:38 +0800

    add aarch64-darwin

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "PTSD development enviroment";
+  description = "PTSD development environment";
   nixConfig.bash-prompt-prefix = "(PTSD)";
 
   inputs = {
@@ -7,39 +7,57 @@
   };
 
   outputs =
-    { self, nixpkgs }:
+    { self, nixpkgs, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      pkgsFor = system: import nixpkgs { inherit system; };
+      lib = nixpkgs.lib;
     in
     {
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
-      packages = {
-        x86_64-linux.default = pkgs.stdenv.mkDerivation {
-          name = "ptsd";
-          # `nix build` fails because of a problem with cmake fetchcontent.
-          # To avoid successful execution followed by failure,
-          # I've commented out `src`.
-          # src = ./.;
-          nativeBuildInputs = [
-            pkgs.cmake
-            pkgs.ninja
-            pkgs.clang
-            # pkgs.SDL2
-            # pkgs.SDL2_image
-            # pkgs.SDL2_ttf
-            # pkgs.SDL2_mixer
-            # pkgs.spdlog
-            # pkgs.glm
-            # pkgs.gtest
-          ];
-          buildInputs = [
-            pkgs.libGLU
-            pkgs.mesa
-            pkgs.xorg.libX11
-            pkgs.xorg.libXext
-          ];
-        };
-      };
+      formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixfmt-rfc-style;
+      packages = lib.genAttrs systems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.stdenv.mkDerivation {
+            name = "ptsd";
+            # `nix build` fails because of a problem with cmake fetchcontent.
+            # To avoid successful execution followed by failure,
+            # I've commented out `src`.
+            # src = ./.;
+            nativeBuildInputs = [
+              pkgs.cmake
+              pkgs.ninja
+              pkgs.clang
+              # pkgs.SDL2
+              # pkgs.SDL2_image
+              # pkgs.SDL2_ttf
+              # pkgs.SDL2_mixer
+              # pkgs.spdlog
+              # pkgs.glm
+              # pkgs.gtest
+            ];
+            buildInputs =
+              [
+                pkgs.libGLU
+                pkgs.mesa
+                pkgs.xorg.libX11
+                pkgs.xorg.libXext
+                pkgs.llvmPackages.llvm
+              ]
+              ++ lib.optionals (system == "aarch64-darwin") [
+                pkgs.darwin.apple_sdk.frameworks.Cocoa
+                pkgs.darwin.apple_sdk.frameworks.IOKit
+                pkgs.darwin.apple_sdk.frameworks.ForceFeedback
+                pkgs.darwin.apple_sdk.frameworks.AVFoundation
+              ];
+          };
+        }
+      );
     };
 }
